763e9d50f56083bed70f763be804ef77ada5151f
BernMasache <bernardmasache>
2023-07-15 17:17:42 +0200

    bug fix

--- components/pages/stockitReportForm.jsx
+++ components/pages/stockitReportForm.jsx
@@ -85,7 +85,7 @@ class CollectionComponent extends React.Component {
                                                 </div> */}
                                                 <div className="mt-3 text-center sm:mt-0  sm:text-left">
                                                     <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 capitalize">
-                                                        Create new collection
+                                                        New collection
                                                     </Dialog.Title>
                                                     <div className="mt-2">
                                                         <p className="text-sm text-gray-500">
