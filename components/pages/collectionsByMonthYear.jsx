f6a131cb597244c46f1251cf67a93ee44a4f2716
BernMasache <bernmasache@gmail.com>
2023-08-01 21:34:07 +0200

    fixed table width

--- components/pages/collectionsByMonthYear.jsx
+++ components/pages/collectionsByMonthYear.jsx
@@ -28,7 +28,9 @@ export default function CollectionsByMonthYear(props) {
                                     <>
                                         <dt>
                                             <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
-                                                <span className="text-base font-semibold leading-7">{date}</span>
+                                                <span className="text-base font-semibold leading-7">{[{ key: "01", month: "January" }, { key: "02", month: "February" }, { key: "03", month: "March" }, { key: "04", month: "April" }, { key: "05", month: "May" }, { key: "06", month: "June" }, { key: "07", month: "July" }, { key: "08", month: "August" },{ key: "09", month: "September" }, { key: "10", month: "October" }, { key: "11", month: "November" }, { key: "12", month: "December" }].map((dayMonth, k) => {
+                                                    return dayMonth.key == date.split("-")[1] ? <div className="">{dayMonth.month}, {date.split("-")[0]}</div> : ""
+                                                })}</span>
                                                 <span className="ml-6 flex h-7 items-center">
                                                     {open ? (
                                                         <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
@@ -39,34 +41,34 @@ export default function CollectionsByMonthYear(props) {
                                             </Disclosure.Button>
                                         </dt>
                                         <Disclosure.Panel as="dd" className="mt-2 pr-1">
-                                            <tbody className="bg-white">
-                                                <table className="min-w-full divide-y divide-gray-300">
-                                                    <thead>
-                                                        <tr>
-                                                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
-                                                                Date
-                                                            </th>
-                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                                Amount
-                                                            </th>
-                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                                Rent/Day
-                                                            </th>
-                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                                No. of Days
-                                                            </th>
-                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                                Total Rent
-                                                            </th>
-                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                                Shop Share
-                                                            </th>
-                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                                Gryton Share
-                                                            </th>
+                                            <table className="min-w-full divide-y divide-gray-300">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                                                            Date
+                                                        </th>
+                                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                            Amount
+                                                        </th>
+                                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                            Rent/Day
+                                                        </th>
+                                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                            No. of Days
+                                                        </th>
+                                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                            Total Rent
+                                                        </th>
+                                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                            Shop Share
+                                                        </th>
+                                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                            Gryton Share
+                                                        </th>
 
-                                                        </tr>
-                                                    </thead>
+                                                    </tr>
+                                                </thead>
+                                                <tbody className="bg-white">
                                                     {
                                                         props?.collections?.map((collection) => {
                                                             return <tr key={collection?.id} className="even:bg-gray-50">
@@ -92,8 +94,8 @@ export default function CollectionsByMonthYear(props) {
                                                         }
                                                         )
                                                     }
-                                                </table>
-                                            </tbody>
+                                                </tbody>
+                                            </table>
                                         </Disclosure.Panel>
                                     </>
                                 )}
