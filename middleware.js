430eb44359d7824feae1b6d25ebd65788b170cca
BernMasache <bernmasache@gmail.com>
2023-11-11 08:05:41 +0200

    login bug

--- middleware.js
+++ middleware.js
@@ -3,31 +3,31 @@ import { NextResponse } from "next/server";
 export async function middleware(request, response) {
   var tokenUser = request.cookies.get("STUD");
 
-  if (tokenUser == undefined || tokenUser == null || tokenUser == "") {
-    if (request.nextUrl.pathname == "/") {
+  if (tokenUser === undefined || tokenUser === null || tokenUser === "") {
+    if (request.nextUrl.pathname === "/") {
       return NextResponse.redirect(new URL("/login", request.url));
     } else if (
-      request.nextUrl.pathname == "/admin" ||
-      request.nextUrl.pathname == "/shop"
+      request.nextUrl.pathname === "/admin" ||
+      request.nextUrl.pathname === "/shop"
     ) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
   } else {
     var token = JSON.parse(tokenUser.value).user;
 
-    if (request.nextUrl.pathname == "/") {
+    if (request.nextUrl.pathname === "/") {
       return NextResponse.redirect(new URL("/login", request.url));
     }
     if (
-      (token == null || token == undefined || token == "") &&
-      (request.nextUrl.pathname == "/admin" ||
-        request.nextUrl.pathname == "/shop")
+      (token === null || token === undefined || token === "") &&
+      (request.nextUrl.pathname === "/admin" ||
+        request.nextUrl.pathname === "/shop")
     ) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
 
     if (
-      (token != null && request.nextUrl.pathname == "/") ||
+      (token !== null && request.nextUrl.pathname === "/") ||
       request.nextUrl.pathname == "/login"
     ) {
       let user = token?.role === "admin" ? "/admin" : "/shop";
